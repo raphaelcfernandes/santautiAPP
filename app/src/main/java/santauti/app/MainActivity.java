d6120d4127852c10d9dd31296438720b5f1ccef0
Raphael Fernandes <raphael.cardoso.f@gmail.com>
2017-04-17 18:00:11 -0300

    First commit

---
+++ app/src/main/java/santauti/app/MainActivity.java
@@ -0,0 +1,33 @@
+package santauti.app;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import santauti.app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText username = (EditText)findViewById(R.id.input_usuario);
+        final EditText password = (EditText)findViewById(R.id.input_password);
+        Button login = (Button)findViewById(R.id.btn_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Toast.makeText(context,"Olá "+username.getText().toString(), Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this,Ficha.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
