5a3a4776cc84ccefadca973261a4a6108f5f4811
Raphael Fernandes <raphael.cardoso.f@gmail.com>
2017-04-18 19:01:43 -0300

    Testando tabs + floating button, com exemplo do android studio

--- app/src/main/java/santauti/app/Ficha.java
+++ app/src/main/java/santauti/app/Ficha.java
@@ -1,5 +1,6 @@
 package santauti.app;
 
+import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -20,19 +21,7 @@ import android.widget.TextView;
 
 public class Ficha extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
 
     @Override
@@ -41,15 +30,22 @@ public class Ficha extends AppCompatActivity {
         setContentView(R.layout.activity_ficha);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
+        setSupportActionBar(toolbar); //Sets the toolbar
+
+        /*
+         * Create the adapter that will return a fragment for each of the three
+         * primary sections of the activity.
+         */
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
+        /*
+         * Set up the ViewPager with the sections adapter.
+        */
         mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setAdapter(mSectionsPagerAdapter);//Objeto da classe SectionsPageAdapter que configura o ViewPager
 
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/santauti/app/MainActivity.java
+++ app/src/main/java/santauti/app/MainActivity.java
@@ -1,15 +1,12 @@
 package santauti.app;
 import android.content.Context;
 import android.content.Intent;
-import android.provider.ContactsContract;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import santauti.app.R;
 
 public class MainActivity extends AppCompatActivity {
 
