9d15df12279638328db029bb05b0bf764c35bcfb
Raphael Fernandes <raphael.cardoso.f@gmail.com>
2017-04-19 15:22:53 -0300

    aprendendo layout

--- app/src/main/java/santauti/app/MainActivity.java
+++ app/src/main/java/santauti/app/MainActivity.java
@@ -14,17 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText username = (EditText)findViewById(R.id.input_usuario);
-        final EditText password = (EditText)findViewById(R.id.input_password);
-        Button login = (Button)findViewById(R.id.btn_login);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                Toast.makeText(context,"Olá "+username.getText().toString(), Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(MainActivity.this,Ficha.class);
-                MainActivity.this.startActivity(intent);
-            }
-        });
+//        final EditText username = (EditText)findViewById(R.id.input_usuario);
+//        final EditText password = (EditText)findViewById(R.id.input_password);
+//        Button login = (Button)findViewById(R.id.btn_login);
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Context context = getApplicationContext();
+//                Toast.makeText(context,"Olá "+username.getText().toString(), Toast.LENGTH_LONG).show();
+//                Intent intent = new Intent(MainActivity.this,Home.class);
+//                MainActivity.this.startActivity(intent);
+//            }
+//        });
     }
 }
