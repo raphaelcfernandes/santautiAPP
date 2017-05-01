23e8c35903694b9ab92d5e39526c3682866bd05a
Raphael Cardoso Fernandes <raphael.cardoso.f@gmail.com>
2017-05-01 17:31:15 -0300

    Arrumei intent e recyclerView

--- app/src/main/java/santauti/app/Controller/Home/HomeAdapter.java
+++ app/src/main/java/santauti/app/Controller/Home/HomeAdapter.java
@@ -1,18 +1,25 @@
 package santauti.app.Controller.Home;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 import santauti.app.Model.HomeModel;
 import santauti.app.R;
+import santauti.app.View.Ficha;
+import santauti.app.View.Home;
 
 /**
  * Created by rapha on 27-Apr-17.
@@ -20,7 +27,8 @@ import santauti.app.R;
 
 public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder> {
     private final List<HomeModel> homeModelList;
-
+    private Context context;
+    private Intent intent;
     public class HomeViewHolder extends RecyclerView.ViewHolder{
         public final TextView nomePaciente;
         public final TextView leito;
@@ -35,8 +43,29 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder
             contactImage = (ImageView)itemView.findViewById(R.id.pacienteImage);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View view) {
-                    Log.d("Print","Cliquei");
+                public void onClick(final View view) {
+                    PopupMenu popupMenu = new PopupMenu(view.getContext(), view, Gravity.NO_GRAVITY, R.attr.actionOverflowMenuStyle, 0);
+                    popupMenu.getMenuInflater().inflate(R.menu.menu_select_ficha,popupMenu.getMenu());
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()){
+                                case R.id.MnuOpc1:
+                                    intent = new Intent(view.getContext(),Ficha.class);
+                                    intent.putExtra("tipoFicha", "Diurna");
+                                    view.getContext().startActivity(intent);
+                                case R.id.MnuOpc2:
+                                    intent = new Intent(view.getContext(),Ficha.class);
+                                    intent.putExtra("tipoFicha", "Noturna");
+
+                                    view.getContext().startActivity(intent);
+                                default:
+                                    return false;
+                            }
+                        }
+                    });
+
+                    popupMenu.show();
                 }
             });
         }
@@ -50,7 +79,11 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.pacientes_view,parent,false);
         return new HomeViewHolder(itemView);
     }
-
+    public void removeItem(int position) {
+        homeModelList.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, homeModelList.size());
+    }
     @Override
     public void onBindViewHolder(HomeViewHolder holder, int position) {
         HomeModel homeModelVO = homeModelList.get(position);
