553516a5925b163f53318b43ad762e74cb89babe
Raphael Cardoso Fernandes <raphael.cardoso.f@gmail.com>
2017-05-13 19:20:52 -0300

    Apenas 1 campo restante hemodinamico. Consultar TODO, iniciar Respiratorio

--- app/src/main/java/santauti/app/View/Ficha/PartesMedicas/HemodinamicoActivity.java
+++ app/src/main/java/santauti/app/View/Ficha/PartesMedicas/HemodinamicoActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 import santauti.app.R;
 
@@ -13,14 +17,22 @@ import santauti.app.R;
 
 public class HemodinamicoActivity extends AppCompatActivity{
     private int x=0;
-    private Intent intent;
-
+    Spinner ritmo,bulhas;
+    RadioButton hemodinamico_opcional_sim,hemodinamico_opcional_nao;
+    private View hemodinamico_opcional_layout;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        intent = getIntent();
-        x=intent.getIntExtra("Position",x);
+        Intent intent = getIntent();
+        x= intent.getIntExtra("Position",x);
         setContentView(R.layout.hemodinamico);
+        findViewById(R.id.hemodinamico_layout).requestFocus();
+        getSupportActionBar().setTitle(R.string.Hemodinamico);
+        hemodinamico_opcional_nao = (RadioButton)findViewById(R.id.hemodinamico_opcional_nao);
+        hemodinamico_opcional_sim = (RadioButton)findViewById(R.id.hemodinamico_opcional_sim);
+        this.hemodinamico_opcional_layout = findViewById(R.id.hemodinamico_opcional_layout);
+        this.hemodinamico_opcional_layout.setVisibility(View.GONE);
+        prepareHemodinamicoSpinners();
         buildIntent(x);
     }
 
@@ -30,5 +42,33 @@ public class HemodinamicoActivity extends AppCompatActivity{
         resultIntent.putExtra("position",x);
         setResult(RESULT_OK, resultIntent);
         return resultIntent;
+    }
+
+    private void prepareHemodinamicoSpinners(){
+        String[] ritmo = {"Ritmico","Aritmico"};
+        String[] bulhas = {"Grau 1","Grau 2","Grau 3","Grau 4","Grau 5","Grau 6"};
+
+        this.ritmo = (Spinner) findViewById(R.id.hemodinamico_ritmo);
+        ArrayAdapter<String> adapterRitmo = new ArrayAdapter<>(HemodinamicoActivity.this, android.R.layout.simple_dropdown_item_1line, ritmo);
+        this.ritmo.setAdapter(adapterRitmo);
+
+        this.bulhas = (Spinner) findViewById(R.id.hemodinamico_bulhas);
+        ArrayAdapter<String> adapterBulhas = new ArrayAdapter<>(HemodinamicoActivity.this, android.R.layout.simple_dropdown_item_1line, bulhas);
+        this.bulhas.setAdapter(adapterBulhas);
+    }
+    public void hemodinamicoOpcionalOnRadioButtonClicked(View view){
+        boolean checked = ((RadioButton) view).isChecked();
+        switch(view.getId()) {
+            case R.id.hemodinamico_opcional_sim:
+                if (checked) {
+                    hemodinamico_opcional_layout.setVisibility(View.VISIBLE);
+                }
+                break;
+            case R.id.hemodinamico_opcional_nao:
+                if (checked) {
+                    hemodinamico_opcional_layout.setVisibility(View.GONE);
+                }
+                break;
+        }
     }
 }
